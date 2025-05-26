9b65e60f0091c8d1a7ff17f70edd347219685e14
ZephyAdonai <139012926+ZephyAdonai@users.noreply.github.com>
2025-05-26 05:46:38 -0400

    Removed sensitive info

--- SC_Functions.py
+++ SC_Functions.py
@@ -47,3 +47,24 @@ async def wait():
     page.wait_for_load_state('load'),
     page.click('#some-button')
     )
+    
+    
+#######################################
+##### SocialMedia object creation #####
+#######################################
+class SM_Object:
+    def __init__(self, logindir, userbox, passbox, submitbox):
+        self.logindir = logindir
+        self.userbox = userbox
+        self.passbox = passbox
+        self.submitbox = submitbox
+        # For login session management
+        self.saved_sessions = []
+    #############################
+    ### Manage login sessions ###
+    #############################
+    def save_session(self,session):
+        session_check = os.path.join(os.getcwd(), f"{session}.json")
+        print(f"Saving session [{session}.json]...")
+        self.saved_sessions.append(session)
+        print(f"Saved session [{session}.json] ")
